0179de50f558e49d1dea26b21cdd232465bc5739
Alexander Schlesinger <alexschlesinger25@gmail.com>
2020-07-11 23:29:23 -0400

    Revert "add dynamic time-availability input addition capability"
    This reverts commit 65aa795dc42aa5a1a5934f161b27dc60be0704d0.
    
    Revert commit to be able to redo commit in a separate branch.

--- sps-team-38/src/main/webapp/script.js
+++ sps-team-38/src/main/webapp/script.js
@@ -10,27 +10,12 @@ function onSignIn(googleUser){
     console.log(profile);
   }
   
-function signOut() {
+  function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
       $("#email").remove();
       userEmail = null;
     });
-}
-
-var counter = 1;
-var limit = 10;
-let today = new Date().toISOString().substr(0,16);
-function addInput(divName){
-    if (counter == limit) {
-        alert("You have reached the limit of adding " + counter + " time availability inputs");
-    }
-    else {
-        var newdiv = document.createElement('div');
-        newdiv.innerHTML = `Entry ${counter+1} <br><input type='datetime-local' id='time_availability' name='time_availability' min='${today}' required>`
-        document.getElementById(divName).appendChild(newdiv);
-        counter++;
-    }
-}  
+  }
   
