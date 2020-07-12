4718f9a9977d0cf90dcdb2b3efe4cb7af09d96f0
roselineshapi <roseshapi81@gmail.com>
2020-07-12 12:32:17 -0400

    add datastore  to store names and timestamps

---
+++ sps-team-38/src/main/java/sps/data/UserInfo.java
@@ -0,0 +1,12 @@
+
+package com.google.sps.data;
+
+public class UserInfo {
+    private final String name;
+    private final long timestamp;
+
+    public UserInfo(String name, long timestamp) {
+        this.name = name;
+        this.timestamp = timestamp;
+    }
+}
--- sps-team-38/src/main/java/sps/servlets/DataServlet.java
+++ sps-team-38/src/main/java/sps/servlets/DataServlet.java
@@ -1,32 +1,59 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package com.google.sps.servlets;
 
+// import com.google.sps.data.ServerStats;
+import com.google.gson.Gson;
 import java.io.IOException;
+import java.util.Date;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.sps.data.UserInfo;
 
-/** Servlet that returns some example content. TODO: modify this file */
+/** Servlet that returns some example content. TODO: modify this file to handle information from user */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
-  }
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Query query = new Query("UserInfo").addSort("timestamp", SortDirection.DESCENDING);
+
+         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        PreparedQuery results = datastore.prepare(query);
+
+        List<UserInfo> name = new ArrayList<>();
+        for (Entity entity : results.asIterable()) {
+            String Name = (String) entity.getProperty("Name");
+            long timestamp = (long) entity.getProperty("timestamp");
+            UserInfo userName = new UserInfo(Name, timestamp);
+            name.add(userName);
+        }
+
+        Gson gson = new Gson();
+        String Json = gson.toJson(name);
+        response.setContentType("application/json;");
+        response.getWriter().println(Json);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //get user input from the form
+        String Name = request.getParameter("Name");
+        long timestamp = System.currentTimeMillis();
+
+        Entity taskEntity = new Entity("UserInfo");
+        taskEntity.setProperty("Name",Name);
+        taskEntity.setProperty("timestamp", timestamp);
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        datastore.put(taskEntity);
+
+    }
 }
