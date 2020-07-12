b4d015a5ec180dfa4a2e974d4a2ff16b483cbd8c
Edem Nuviadenu <edemnuviadenu@gmail.com>
2020-07-12 03:45:41 -0400

    Add Faceted search logic by unique title

--- sps-team-38/src/main/webapp/script.js
+++ sps-team-38/src/main/webapp/script.js
@@ -53,3 +53,51 @@ function addInput(divName){
     }
 }   
 
+const formListElement = document.getElementById('form-list');
+const list = document.querySelector('#form-list ul');
+  // Load existing forms in database
+function loadForms(){
+    fetch('/list-forms').then(response => response.json()).then((forms) => {
+    forms.forEach((form) => {
+        formListElement.appendChild(renderForm(form));
+    })
+    });
+}
+// ................remove this section, just for reference....................
+function renderForm(form){
+    const forms = document.forms;
+    // create elements
+    const li = document.createElement('li');
+    const formName = document.createElement('span');
+    const applyBtn = document.createElement('span');
+
+    // add text content
+    formName.textContent = form.data().title;
+    applyBtn.textContent = 'apply';
+
+    // add classes
+    formName.classList.add('name');
+    applyBtn.classList.add('apply');
+
+    // append to DOM
+    li.appendChild(formName);
+    li.appendChild(applyBtn);
+    list.appendChild(li);
+    
+    //TODO add feature to hide forms
+}
+
+// faceted search logic
+const searchBar = forms['search-forms'].querySelector('input');
+searchBar.addEventListener('keyup', (e) => {
+  const term = e.target.value.toLowerCase();
+  const forms = list.getElementsByTagName('li');
+  Array.from(forms).forEach((form) => {
+    const title = form.firstElementChild.textContent;
+    if(title.toLowerCase().indexOf(term) != -1){
+      form.style.display = 'block';
+    } else {
+      form.style.display = 'none';
+    }
+  });
+});
