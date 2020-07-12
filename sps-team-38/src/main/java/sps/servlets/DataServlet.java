713428eaf961b548a663913988830988d96c762a
roselineshapi <rshapi@usapglobal.org>
2020-07-12 14:27:12 -0400

    renamed variables in the DataServlet.java file

--- sps-team-38/src/main/java/sps/servlets/DataServlet.java
+++ sps-team-38/src/main/java/sps/servlets/DataServlet.java
@@ -1,21 +1,6 @@
 
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package com.google.sps.servlets;
 
-// import com.google.sps.data.ServerStats;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.Date;
@@ -32,7 +17,6 @@ import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.sps.data.UserInfo;
-
 
 /** Servlet that returns some example content. TODO: modify this file to handle information from user */
 @WebServlet("/data")
@@ -70,6 +54,5 @@ public class DataServlet extends HttpServlet {
         taskEntity.setProperty("timestamp", timestamp);
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         datastore.put(taskEntity);
-       
     }
 }
